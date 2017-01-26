bafa5b6f57dff45764e518ff538781f05c54b963
jhover <jhover@cloudy.rhic.bnl.gov>
2017-01-26 13:17:23 -0500

    Update.

--- misc/ssca/ssca.sh
+++ misc/ssca/ssca.sh
@@ -42,32 +42,32 @@ create_intermediate_ca(){
  touch intermediate/index.txt
  echo 1000 > intermediate/serial
  echo 1000 > intermediate/crlnumber
-echo "Making intermediate CA key..."
+ 
+ echo "Making intermediate CA key..."
  openssl genrsa -aes256 \
     -out intermediate/private/intermediate.key.pem 4096
  chmod 400 intermediate/private/intermediate.key.pem
 
-echo "Making intermediate request..."
-
+ echo "Making intermediate request..."
  openssl req -config intermediate/openssl.cnf -new -sha256 \
       -key intermediate/private/intermediate.key.pem \
       -out intermediate/csr/intermediate.csr.pem		
 
-echo "Signing intermediate request, generating intermeidate..."
+ echo "Signing intermediate request, generating intermeidate..."
  openssl ca -config root/openssl.cnf -extensions v3_intermediate_ca \
       -days 3650 -notext -md sha256 \
       -in intermediate/csr/intermediate.csr.pem \
       -out intermediate/certs/intermediate.cert.pem
  chmod 444 intermediate/certs/intermediate.cert.pem																																		
 
-echo "Emitting intermediate cert..."
+ echo "Emitting intermediate cert..."
  openssl x509 -noout -text \
       -in intermediate/certs/intermediate.cert.pem
 
-echo "Verifying intermdiate cert..."
+ echo "Verifying intermdiate cert..."
  openssl verify -CAfile root/certs/ca.cert.pem \
       intermediate/certs/intermediate.cert.pem
-echo "Done."
+ echo "Done."
 }
 
 
@@ -84,27 +84,80 @@ echo "Done"
 create_host_certificate(){
  hostname=`hostname -f`
 
+ echo "Generating new private key for host cert..."
+ openssl genrsa -aes256 \
+    -out intermediate/private/$hostname.key.pem 2048
+ chmod 400 intermediate/private/$hostname.key.pem
+openssl rsa -in intermediate/private/$hostname.key.pem \
+  -out intermediate/private/$hostname.keynopw.pem
+chmod 400 intermediate/private/$hostname.keynopw.pem
+
+ echo "Creating CSR for host cert using new private key..."
  openssl req -config intermediate/openssl.cnf \
     -key intermediate/private/$hostname.key.pem \
       -new -sha256 -out intermediate/csr/$hostname.csr.pem
 
+ echo "Signing CSR with intermediate private key..."
  openssl ca -config intermediate/openssl.cnf \
       -extensions server_cert -days 375 -notext -md sha256 \
       -in intermediate/csr/$hostname.csr.pem \
       -out intermediate/certs/$hostname.cert.pem
-
  chmod 444 intermediate/certs/$hostname.cert.pem
 
+ echo "Verifying new host certificate..."
  openssl x509 -noout -text \
     -in intermediate/certs/$hostname.cert.pem
 
+echo "Done."
+}
+
+create_crl(){
+	echo "Creating CRL..."
 }
 
 
+create_user_certificate(){
+echo "Argument length is $#"
+		
+if [[ "$#" -eq "0" ]] ;   then
+    echo "usercert requires Subject name argument."
+    exit 1
+fi
+
+ username=$1
+ echo "Creating user certificate for $username ..."
+ echo "Generating new private key for user cert..."
+ openssl genrsa -aes256 \
+    -out intermediate/private/$username.key.pem 2048
+ chmod 400 intermediate/private/$username.key.pem
+openssl rsa -in intermediate/private/$username.key.pem \
+-out intermediate/private/$username.keynopw.pem
+chmod 400 intermediate/private/$username.keynopw.pem
+
+
+ echo "Creating CSR for user cert using new private key..."
+ openssl req -config intermediate/openssl.cnf \
+    -key intermediate/private/$username.key.pem \
+      -new -sha256 -out intermediate/csr/$username.csr.pem
+
+ echo "Signing CSR with intermediate private key..."
+ openssl ca -config intermediate/openssl.cnf \
+    -extensions usr_cert -days 375 -notext -md sha256 \
+      -in intermediate/csr/$username.csr.pem \
+      -out intermediate/certs/$username.cert.pem
+ chmod 444 intermediate/certs/$username.cert.pem
+
+ echo "Verifying new user certificate..."
+ openssl x509 -noout -text \
+    -in intermediate/certs/$username.cert.pem
+
+ echo "Done."
+}
+
 clean(){
-	rm -f */certs/*.pem
+ rm -f */certs/*.pem
  rm -f */private/*.pem	
-rm -f */csr/*.pem
+ rm -f */csr/*.pem
 }
 
 
@@ -124,18 +177,18 @@ certchain)
 
 hostcert)
     create_host_certificate
-        RETVAL=$?
-        ;;
+    RETVAL=$?
+    ;;
 usercert)
-    create_user_certificate
-        RETVAL=$?
-        ;;
+    create_user_certificate $2
+    RETVAL=$?
+    ;;
 clean)
      clean
      RETVAL=$?
      ;;
 *)
-        echo $"Usage: $0 {createroot|createintermediate|certchain|hostcert|usercert|clean}"
+        echo $"Usage: $0 {createroot|createintermediate|certchain|crl|hostcert|usercert|clean}"
         RETVAL=2
 esac
 
